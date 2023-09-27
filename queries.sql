d4f950b01f616873b455573a9a9dc2f87618ff7f
kazim110 <kazim.mohammadi110@gmail.com>
2023-09-27 19:35:18 +0430

    Correct some mistake in SELECT and INSERT

--- queries.sql
+++ queries.sql
@@ -2,14 +2,14 @@
 
 SELECT * from animals WHERE name = 'mon';
 
-SELECT id, name, date_of_birth FROM animals WHERE '2019-12-30'> date_of_birth > '2016-01-01';
+SELECT id, name, date_of_birth FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-30';
 
 SELECT id, name, escape_attempts FROM animals WHERE escape_attempts < 3;
 
-SELECT id, name, date_of_birth FROM animals WHERE namd IN ('Agumon','Pikachu');
+SELECT id, name, date_of_birth FROM animals WHERE name IN ('Agumon','Pikachu');
 
 SELECT id, name, escape_attempts, weight_kg FROM animals WHERE weight_kg > 10.5;
 
 SELECT * FROM animals WHERE neutered = true;
-SELECT * FROM animals WHERE name NOT 'Gabumon';
-SELECT * FROM animals WHERE 10,4 <= weight_kg <= 17,3;
+SELECT * FROM animals WHERE name NOT IN ('Gabumon');
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
